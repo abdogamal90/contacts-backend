8cdd64e7e89026cefebcdf285ba30a9d26cb7cc4
Abdelrahman Gamal <62604408+abdogamal90@users.noreply.github.com>
2025-06-07 14:31:11 +0300

    feat : added pagination to contacts list

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,23 +1,28 @@
 import Contact from '../models/Contact.js';
 export const getContacts = async (req, res) => {
 
+  const page = 1;
+  const limit = 5;
+  const offset = (page - 1) * limit;
+
   try {
-    const contacts = await Contact.find();
-    // const count = await Contact.countDocuments();
-    res.json({ contacts });
+    const contacts = await Contact.find().skip(offset).limit(limit).exec();
+    const totalContacts = await Contact.countDocuments({});
+    const totalPages = Math.ceil(totalContacts / limit);
+    return res.status(200).json({ totalContacts, page, totalPages, contacts });
   } catch (err) {
     res.status(500).json({ error: 'Server error' });
   }
 };
 
 export const createContact = async (req, res) => {
-  const { name, phone, address, notes } = req.body;
-  if (!name || !phone || !address) {
+  const data = req.body;
+  if (!data.name || !data.phone || !data.address) {
     return res.status(400).json({ error: 'All fields are required' });
   }
 
   try {
-    const newContact = new Contact({ name, phone, address, notes });
+    const newContact = new Contact(data);
     await newContact.save();
     res.status(201).json(newContact);
   } catch (err) {
